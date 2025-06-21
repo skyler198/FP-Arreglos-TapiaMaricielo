dac31fb5e83aebfb5715d1dca35b8f6c9c039af2
maricielo <cielotapia198@gmail.com>
2025-06-21 00:13:37 -0500

    Ejercicio 1 – Implementación de la lógica para convertir en mayúsculas a las palabras iniciales y mejorar de la interfaz de usuario, versión final

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -11,12 +11,32 @@ void mayusculaPalabras(string &oracion){
 		n++;
 	}
 	//verificar espacios y poner mayusculas
+	for (int i = 0; i < n; i++){
+		if (espacio){
+			oracion[i] = toupper (oracion[i]);
+			espacio = false;
+			
+		} else if (oracion[i] == ' '){
+			espacio = true;
+		}
+	}
 }
 
 int main (){
 	string oracion;
-	cout << "escriba una oracion: " << endl;
+	
+	cout << "==========================================================" << endl;
+	cout << "              BIENVENIDO A MI PROGRAMA!" << endl;
+	cout << "==========================================================\n" << endl;
+	
+	cout << "Escriba una oracion: " << endl;
 	getline(cin, oracion);
+	
+	mayusculaPalabras(oracion);
+	
+	cout << "--> " << oracion << endl;
+	
+	cout << "\n          GRACIAS POR USAR MI PORGRAMA <3!" << endl;
 	
 	return 0;
 }
