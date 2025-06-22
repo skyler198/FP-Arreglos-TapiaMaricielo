731d52e2ebdd8ee1efc79b4ca2edc7e943df2a69
maricielo <cielotapia198@gmail.com>
2025-06-21 23:29:53 -0500

    Ejercicio 4 – Agregar logica para ordenar alfabeticamente nombres (metodo de la burbuja), mostrar resultados y mejorar interfaz del usuario, version final

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main() {
     int cantidad;
-
+    string aux;
+    cout << "==================================================" << endl;
+    cout << "  PROGRAMA PARA ORDENAR NOMBRES ALFABETICAMENTE" << endl;
+	cout << "==================================================" << endl;
+		
     cout << "Cuantos nombres desea ingresar?: ";
     cin >> cantidad;
     cin.ignore();
@@ -17,6 +21,21 @@ int main() {
     	getline(cin, nombres[i]);
 	}
 	//logica para ordenarlos alfabeticamente
+	for (int i = 0; i < cantidad - 1; i++){
+		for(int j = i + 1; j < cantidad; j++){
+			if (nombres[i] > nombres[j]){
+				aux = nombres[i];
+				nombres[i] = nombres[j];
+				nombres[j] = aux;
+			}
+		}
+	}
 	//mostrar resultados
-}
+	cout << "\nNombres ordenados alfabeticamente: \n";
+	for (int i = 0; i < cantidad; i++){
+		cout << "Nombre " << i + 1 << ": " << nombres[i] << endl;
+	}
+	cout << "\n        GRACIAS POR USAR ESTE PROGRAMA <3!" << endl;
+	return 0;
+}s
 
