d409c7cd4fff2d7eed66f0b39a2ea2a4f6b0a32d
maricielo <cielotapia198@gmail.com>
2025-06-22 01:02:07 -0500

    Ejercicio 5 – Buscar numero mayor, menor y contar sus repeticiones, mostrar los resultados y mejorar interfaz del usuario, versión final

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -6,17 +6,47 @@ using namespace std;
 
 int main(){
 	int n;
+	int max, min;
+	int repm = 1, repM = 1;
+	
+	cout << "==========================================================" << endl;
+	cout << "              BIENVENIDO A MI PROGRAMA!" << endl;
+	cout << "==========================================================\n" << endl;
 
     cout << "Cuantos numeros desea ingresar?: ";
     cin >> n;
     
     int numeros[n];
     
+    //lectura de numeros
     for(int i = 0; i < n; i++){
     	cout << "Numero " << i + 1 <<": ";
     	cin >> numeros[i];
 	}
 	//encontrar el mayor y menor numero
-	//contar las repeticiones
+	max = numeros[0];
+	min = numeros[0];
+	
+	for (int i = 1; i < n; i++) {
+        if (numeros[i] > max) {
+            max = numeros[i];
+            repM = 1; //inicializar de nuevo por que ya aprecieron una vez
+        } else if (numeros[i] == max) {
+            repM++; //contar las repeticiones
+        }
+        
+        if (numeros[i] < min) {
+            min = numeros[i];
+            repm = 1; //inicializar de nuevo por que ya aprecieron una vez
+        } else if (numeros[i] == min) {
+            repm++; //contar las repeticiones
+        }
+    }
 	//Mostrar resultados
+	cout << "\nNumero mayor: " << max << " (se repite " << repM << " veces)" << endl;
+    cout << "Numero menor: " << min << " (se repite " << repm << " veces)" << endl;
+    
+    cout << "\n          GRACIAS POR USAR MI PORGRAMA <3!" << endl;
+    
+    return 0;
 }
