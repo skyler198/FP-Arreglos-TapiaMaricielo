9720b80367990dde72b3f50c0553700724a72cc7
maricielo <cielotapia198@gmail.com>
2025-06-21 20:00:32 -0500

    Ejercicio 3 – Agregar funcion principal main (pedir datos y mostrarlos) y mejorar la interfaz del usuario con una bienvenida y agradecimiento, versión final

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -17,3 +17,25 @@ int ContarConcurrenciaPalabra( string oracion, char n){
 	return cont;
 }
 
+int main (){
+	string oracion;
+	char n;
+	int total;
+		
+	cout << "==========================================================" << endl;
+	cout << "              BIENVENIDO A MI PROGRAMA!" << endl;
+	cout << "==========================================================\n" << endl;
+	
+	cout << "Escriba una oracion: ";
+	getline(cin, oracion);
+	cout << "Que letra desea verificar?: ";
+	cin >> n;
+	
+	total = ContarConcurrenciaPalabra(oracion, n );
+	
+	cout << "La cantidad de '" << n << "' que hay en la oracion es de: " << total << endl; 
+	
+	cout << "\n          GRACIAS POR USAR MI PORGRAMA <3!" << endl;
+	
+	return 0;
+}
