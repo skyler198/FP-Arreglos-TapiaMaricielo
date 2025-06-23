7f675002d0fa81612828077c8c78ff72832e7fc9
maricielo <cielotapia198@gmail.com>
2025-06-22 20:21:16 -0500

    Ejercicio 6 – mejorar la interfaz del usuario, versión final

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -7,13 +7,17 @@ int main(){
 	int n;
 	int impar = 0, par = 0;
 	
+	cout << "================================================================" << endl;
+	cout << "                   BIENVENIDO A MI PROGRAMA" << endl;
+	cout << "================================================================" << endl;
+	
 	cout << "ingrese la cantidad de numeros que contendra su vector: ";
 	cin >> n;
 	
 	int numeros[n];
 	//lectura de numeros
 	for (int i = 0; i < n; i++){
-		cout << "Numero " << i + 1 << " :";
+		cout << "Numero " << i + 1 << " : ";
 		cin >> numeros[i];
 	}
 	
@@ -27,7 +31,9 @@ int main(){
 	}
 	
 	//mostrar resultados nwn!
-	cout << par << endl;
-	cout << impar << endl;
-
+	cout << "\nLa suma de numeros PARES es: " << par << endl;
+	cout << "La suma de numeros IMPARES es: " <<impar << endl;
+	
+	cout << "\n         GRACIAS POR USAR MI PROGRAMA, VUELVA PRONTO!" << endl;
+	return 0;
 }
