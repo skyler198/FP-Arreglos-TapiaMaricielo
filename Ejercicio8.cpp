4f9a5bef5bdc7137aa0144f4605123b813dfcd93
maricielo <cielotapia198@gmail.com>
2025-06-23 00:27:56 -0500

    Ejercicio 8 – avance 2: agregar validacion para que el digito este entre 0 y 9 y agregar logica para determinar si es capicua

--- Ejercicio8.cpp
+++ Ejercicio8.cpp
@@ -8,11 +8,33 @@ int main(){
 
     cout << "Ingrese los 10 digitos de su numero:" << endl;
 	//leer digitos
+	//verficar que sean digitos entre 0 y 9
+	
     for (int i = 0; i < 10; i++) {
-        cout << "Digito " << i + 1 << ": ";
-        cin >> digitos[i];
+        int dig;
+        do {
+            cout << "Digito " << i + 1 << ": ";
+            cin >> dig;
+
+            if (dig < 0 || dig > 9) {
+                cout << "Digito invalido, cada digito debe estar entre 0 y 9" << endl;
+            }
+
+        } while (dig < 0 || dig > 9);
+
+        digitos[i] = dig;
     }
-    //verficar que sean digitos entre 0 y 9
+    
     // logica para ver si el numero es capicua
+    bool esCapicua = true;
+    
+    for (int i = 0; i < 5; i++) { // evaluamos los 5 primeros digitos porque es la mitad de cifras 
+        if (digitos[i] != digitos[9 - i]) {
+            esCapicua = false;
+            break;
+        }
+    }
+    
     //mostrar resultados
+
 }
