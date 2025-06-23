f53dda245a5b0c48618e1b5f1494e8293642bec2
maricielo <cielotapia198@gmail.com>
2025-06-23 00:53:24 -0500

    Ejercicio 9 – avance 1: lectura de numeros

---
+++ Ejercicio9.cpp
@@ -0,0 +1,21 @@
+/*Rotar las componentes de un vector una posición hacia la derecha*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout << "ingrese la cantidad de numeros que contendra su vector: ";
+	cin >> n;
+	
+	int numeros[n];
+	
+	//lectura de numeros
+	for (int i = 0; i < n; i++){
+		cout << "Numero " << i + 1 << " : ";
+		cin >> numeros[i];
+	}
+	//mostar vector actual
+	//logica para rotar componentes
+	//mostrar vector ya rotado una posicion a la derecha
+}
